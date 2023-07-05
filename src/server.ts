541820d9ed29348c24b1187512d0868b91662618
YinuoM <mengyn1999@gmail.com>
2023-07-05 17:32:46 +0100

    Handle errors

--- src/server.ts
+++ src/server.ts
@@ -21,4 +21,15 @@ app.use('/api', protect, router);
 app.post('/user', createNewUser);
 app.post('/signin', signin);
 
+// Error handling
+app.use((err, req, res, next) => {
+  if (err.type === 'auth') {
+    res.status(401).json({ message: 'unauthorized' });
+  } else if (err.type == 'input') {
+    res.status(400).json({ message: 'invalid input' });
+  } else {
+    res.status(500).json({ message: 'server error' });
+  }
+});
+
 export default app;
